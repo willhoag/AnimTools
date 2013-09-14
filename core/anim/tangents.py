f286c93850f374e5570691914506e0ebdec1ab1c
willhoag <willhoag@willhoag.com>
2013-09-14 02:35:25 -0700

    Adding tangents module

---
+++ core/anim/tangents.py
@@ -0,0 +1,54 @@
+import maya.cmds as cmds
+from itertools import cycle
+from wh.core.util.strings import userMessage
+
+
+class DefaultTangents:
+
+    '''A class for managing the default tangent types'''
+
+    def __init__(self):
+        self.current = dict()
+        self.update()
+
+    def set(self, itt="auto", ott="auto"):
+        '''For setting the default tangent types'''
+        cmds.keyTangent(g=True, itt=itt, ott=ott)
+        self.update()
+
+    def update(self):
+        '''To update the classes attributes to match the current default tangent types'''
+        current = cmds.keyTangent(q=True, g=True)[0:2]
+        self.current['itt'] = current[0]
+        self.current['ott'] = current[1]
+
+    def toggle(self, *toggles):
+        '''
+        Takes an arbitrary amount of tupples for in tangent and out tangent and cycles through them.
+        Example Usage: DefaultTangents().toggle(('auto', 'auto'), ('clamped', 'step'))
+        - This toggles between auto tangents and stepped tangents  
+        '''
+        self.update()
+
+        # set default
+        tangents = toggles[0]
+
+        # need in order to get next value in loop
+        cycleToggles = cycle(toggles)
+
+        # prime the pump
+        next = cycleToggles.next()
+
+        # reset tangents to next one if it's at one already
+        for toggle in toggles:
+            next = cycleToggles.next()
+            if self.current['ott'] == toggle:
+                tangents = next
+
+        # set default tangents type
+        self.set(itt=tangents[0], ott=tangents[1])
+
+        userMessage("Default tangents set to " + tangents)
+
+        # update class
+        self.update()
