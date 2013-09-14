b37b09cd2597fcbdf9d0254120d4d186c7cde110
willhoag <willhoag@willhoag.com>
2013-09-13 21:45:04 -0700

    Moved the factored code to a shelf module for shelf related items

--- core/general/shelf.py
+++ core/general/shelf.py
@@ -1,16 +1,28 @@
 import maya.cmds as cmds
 import maya.mel as mel
+from wh.util.strings import stripPrefix
 
 
-def reselect():
-    selected = cmds.ls(sl=True)
-    command = 'import maya.cmds as cmds\ncmds.select(%s, add=True)' % selected
-    makeShelfButton(command)
-
-
-def makeShelfButton(command):
+def makeShelfButton(command, sourceType='python'):
     shelfLayout = mel.eval('string $temp = $gShelfTopLevel')
     shelf = cmds.tabLayout(shelfLayout, query=True, st=True)
     cmds.shelfButton(annotation='Saved selection',
-                     image1='menuIconWindow.png', sourceType='python',
+                     image1='menuIconWindow.png', sourceType=sourceType,
                      command=command, parent=shelf)
+
+
+# Custom Buttons
+def reselect(add=False):
+    selected = cmds.ls(sl=True)
+    command = 'import maya.cmds as cmds\ncmds.select(%s, add=%s)' % (selected, add)
+    makeShelfButton(command)
+
+
+def reselectWithPrefixs(add=False):
+    selection = cmds.ls(sl=True)
+    names = []
+    for select in selection:
+        names.append(stripPrefix(select, '_'))
+    import pdb; pdb.set_trace()
+    command = 'wh.selectWithPrefixs(%s, add=%s)' % (names, add)
+    makeShelfButton(command)
